40612edee8f200e85391bf4fa43ece3862fdf7f4
Job van Achterberg <mail@3ode.nl>
2020-05-19 10:14:34 +0000

    Resource collection controller scaffolding

--- wp-content/plugins/imageshare/php/classes/controllers/class.resource_collection.php
+++ wp-content/plugins/imageshare/php/classes/controllers/class.resource_collection.php
@@ -4,12 +4,16 @@ namespace Imageshare\Controllers;
 
 require_once imageshare_php_file('classes/class.logger.php');
 require_once imageshare_php_file('classes/models/class.resource_collection.php');
-require_once imageshare_php_file('classes/views/class.resource_collection.php');
 
 use Imageshare\Logger;
 use ImageShare\Models\ResourceCollection as Model;
-use ImageShare\Views\ResourceCollection as View;
 
 class ResourceCollection {
 
+    public function __construct() {
+    }
+
+    public function get_popular_categories($number = 5) {
+        return array();
+    }
 }
