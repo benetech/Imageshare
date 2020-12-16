1d61b3abe7223520beb8cc7c0e44c716ec789c12
Job van Achterberg <mail@3ode.nl>
2020-12-16 18:55:35 +0000

    Render collection members

--- wp-content/plugins/imageshare/php/classes/controllers/json_api/class.collections.php
+++ wp-content/plugins/imageshare/php/classes/controllers/json_api/class.collections.php
@@ -8,6 +8,25 @@ use Imageshare\Logger;
 use Imageshare\Models\ResourceCollection as Collection;
 
 class Collections extends Base {
+    const plural_name = 'collections';
+
+    public static function members($id, $members) {
+        $data = array_map(function ($member) {
+            return [
+                'type' => 'resource',
+                'id' => (string) $member->id
+            ];
+        }, $members);
+
+        return [
+            'links' => [
+                'self' => parent::relationship_link($id, 'members'),
+                'related' => parent::resource_link($id, 'members')
+            ],
+            'data' => count($data) > 1 ? $data : $data[0],
+        ];
+    }
+
     public static function render() {
         $posts = get_posts([
             'post_type' => 'btis_collection',
@@ -18,16 +37,28 @@ class Collections extends Base {
         $collections = array_reduce($posts, function ($list, $post) {
             $collection = Collection::from_post($post);
 
-            $list[] = [
+            $data = [
                 'type' => 'collection',
                 'id' => (string) $collection->id,
                 'attributes' => [
                     'title' => $collection->title,
                     'description' => $collection->description,
                     'featured' => $collection->is_featured,
+                    'contributor' => $collection->contributor,
+                    'thumbnail' => $collection->thumbnail,
                     'size' => count($collection->resource_ids)
                 ]
             ];
+
+            $resources = $collection->resources();
+
+            if (count($resources)) {
+                $data['relationships'] = [
+                    'members' => self::members($collection->id, $resources)
+                ];
+            }
+
+            $list[] = $data;
 
             return $list;
         }, []);
