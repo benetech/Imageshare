9e5db6afaa6ec269885c7217ac2c7b3fb45efaac
Job van Achterberg <mail@3ode.nl>
2021-06-08 08:09:50 +0000

    Filter keywords through list of stopwords

--- wp-content/plugins/imageshare/php/classes/controllers/json_api/class.keywords.php
+++ wp-content/plugins/imageshare/php/classes/controllers/json_api/class.keywords.php
@@ -9,7 +9,11 @@ class Keywords extends Base {
     const plural_name = 'keywords';
 
     public static function render() {
-        $keywords = ResourceModel::get_keywords();
+        $stopwords = json_decode(file_get_contents(imageshare_asset_file('stopwords.json')));
+
+        $keywords = array_values(array_filter(ResourceModel::get_keywords(), function ($keyword) use ($stopwords) {
+            return !in_array($keyword, $stopwords);
+        }));
 
         return parent::render_response($keywords);
     }
